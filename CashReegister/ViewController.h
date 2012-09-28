37c32026d52bf8b85d66fefbb492893831accb42
Eric Levy <ericlevy@ericlevy.com>
2012-09-28 01:54:32 -0400

    Basic input of Purchase price and cash given, and the display cash back in denominations, amount, and total.
    Remaining to be done:
    1. ensure only currency entry, i.e. numbers, one decimal, and two
    positions to the right of the decimal.
    2. allow sorting to be Alphabetical name by denominations rather than,
    or in addition to, the value of the denomination.  3.  Handle plurals
    better, e.g. PENNIES, not PENNYS
    4.  Change tableview to a single concatenated text field, as per
    specifications
    5.  Title-bar and icons would be nice.

--- CashReegister/ViewController.h
+++ CashReegister/ViewController.h
@@ -7,7 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
+#include "CashReegisterCalculator.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (retain, nonatomic) CashReegisterCalculator *cashregister;
+@property (retain, nonatomic) NSArray *arrCashback;
+
+@property (weak, nonatomic) IBOutlet UITextField *uitxtPurchasePrice;
+@property (weak, nonatomic) IBOutlet UITextField *uitxtCashGiven;
+@property (weak, nonatomic) IBOutlet UITableView *tableviewChange;
+@property (weak, nonatomic) IBOutlet UILabel *uilblCashInRegister;
 
 @end
--- CashReegister/ViewController.m
+++ CashReegister/ViewController.m
@@ -10,18 +10,48 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
+@synthesize cashregister = _cashregister;
+@synthesize arrCashback = _arrCashback;
+
+
+// getters and setters
+-(CashReegisterCalculator *) cashregister
+{
+    if (!_cashregister)
+    {
+        _cashregister = [[CashReegisterCalculator alloc] initWithDefaultCashRoll];
+    }
+    return _cashregister;
+}
+
+-(NSArray *) arrCashback
+{
+    if (!_arrCashback)
+    {
+        _arrCashback = [[NSArray alloc] init];
+    }
+    return _arrCashback;
+}
+
+
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.uilblCashInRegister.text = [NSString stringWithFormat:@"%.2f", [self.cashregister totalCashInRoll]];
 }
 
 - (void)viewDidUnload
 {
+    [self setTableviewChange:nil];
+    [self setUitxtPurchasePrice:nil];
+    [self setUitxtCashGiven:nil];
+    [self setUilblCashInRegister:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -29,6 +59,62 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+// UITableViewDataSource Delegate Messages
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return [NSString stringWithFormat:@"Total Cash Back: $%.2f", [CashReegisterCalculator totalCashBackFromCashBackList:self.arrCashback]];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.arrCashback count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[CashReegisterCalculator cellCacheIdentifier]];
+    
+    if (!cell)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:[CashReegisterCalculator cellCacheIdentifier]];
+    }
+    
+    id details = [[self.arrCashback objectAtIndex:indexPath.row] objectAtIndex:1];
+    if ( [details isKindOfClass:[NSString class]] )
+    {
+        cell.textLabel.text = [[self.arrCashback objectAtIndex:indexPath.row] objectAtIndex:0];  // that's the description of the denomination
+        cell.detailTextLabel.text = details;
+    }
+    else if ( [details isKindOfClass:[NSNumber class]] )
+    {
+        cell.textLabel.text = [NSString stringWithFormat:@"%@ %@%@ back", details, [[self.arrCashback objectAtIndex:indexPath.row] objectAtIndex:0], ([details intValue] == 1 ? @"" : @"S")];  // that's the description of the denomination and the quanity
+        cell.detailTextLabel.text = [NSString stringWithFormat:@"%.2f dollars", [[[self.arrCashback objectAtIndex:indexPath.row] objectAtIndex:2] floatValue]];
+    }
+    return cell;
+ 
+}
+
+- (IBAction)showCashBack:(UIButton *)sender
+{
+    [self.uitxtCashGiven resignFirstResponder];
+    [self.uitxtPurchasePrice resignFirstResponder];
+    
+    self.arrCashback = [[NSArray alloc] initWithArray:[self.cashregister cashBackForPurchasePrice:[self.uitxtPurchasePrice.text floatValue] andCashGiven:[self.uitxtCashGiven.text floatValue]]];
+    
+
+    self.uilblCashInRegister.text = [NSString stringWithFormat:@"%.2f", [self.cashregister totalCashInRoll]];
+    [self.tableviewChange reloadData];
+}
+
+- (IBAction)resreshRegister:(UIButton *)sender
+{
+    [self.cashregister loadWithDefaultCashRoll];
+    self.arrCashback = nil;
+    self.uilblCashInRegister.text = [NSString stringWithFormat:@"%.2f", [self.cashregister totalCashInRoll]];
+    [self.tableviewChange reloadData];
 }
 
 @end
